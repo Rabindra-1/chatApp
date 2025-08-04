037953fd5f14b08c35ded33962b61af71d0ac9e4
Rabindra-1 <sahrabindra394@gmail.com>
2025-08-04 16:16:56 +0545

    backend

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js"
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt
+        if (!token) {
+            return res.status(401).json({ message: "No token provided" })
+        }
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!decode) {
+            return res.status(401).json({ message: "Token is Invalid" });
+        }
+        const user = await User.findById(decode.userId).select("-password");
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        console.log("Error")
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500'>Hello World</div>
+  )
+}
+
+export default App
